1c399bad37311b3815a964aa0d0193d74e1b9667
LucasFournier-GIT <71459382+LucasFournier-GIT@users.noreply.github.com>
2020-11-12 20:05:57 +0100

    Update script_remplissage.sql

--- script_remplissage.sql
+++ script_remplissage.sql
@@ -1,18 +1,14 @@
 --En cours d'édition--
 IF NOT EXISTS (
-    SELECT TOP 1 [Id]
-    FROM [dbo].[Post_PST]
+    SELECT TOP 1 [id]
+    FROM [dbo].[vehicule]
 )
 BEGIN
-    INSERT INTO [parc_auto].[dbo].[Post_PST] ([Titre], [Contenu], [Publication])
-    VALUES  (N'Mon titre', N'Mon contenu', GETDATE()),
-            (N'Autre titre', N'Autre contenu', DATEADD(hh, 1, GETDATE()))
+    INSERT INTO [parc_auto].[dbo].[vehicule] ([color], [date_premiere_circulation], [kilometrage], [disponibilite])
+    VALUES  (N'Rouge', GETDATE(), 120000, TRUE),
+            (N'Verte', DATEADD(hh, 1, GETDATE()), 90000, FALSE)
 
-    DELETE FROM [dbo].[Post_PST]
-    WHERE       [Id] = 1;
-
-    UPDATE  [dbo].[Post_PST]
-    SET     [Revision] = DATEADD(hh, -1, GETDATE())
-    WHERE   [Revision] IS NULL AND [Id] % 2 = 0
+    DELETE FROM [dbo].[vehicule]
+    WHERE       [id] = 1;
 END
 GO
