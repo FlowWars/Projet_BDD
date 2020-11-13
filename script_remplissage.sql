826c7b5a709df8b5e4977f57c0c78346e88b6b15
LucasFournier-GIT <71459382+LucasFournier-GIT@users.noreply.github.com>
2020-11-13 19:31:25 +0100

    Mise à jour du nom des variables

--- script_remplissage.sql
+++ script_remplissage.sql
@@ -8,7 +8,7 @@ IF NOT EXISTS (
     FROM [dbo].[vehicule]
 )
 BEGIN
-    INSERT INTO [parc_auto].[dbo].[vehicule] ([color], [dateFirstCirculation], [kilometrage], [disponibilite])
+    INSERT INTO [parc_auto].[dbo].[vehicule] ([couleur_vehicule], [premiere_mise_en_circulation], [kilometre_vehicule], [disponibilite_vehicule])
     VALUES  (N'Rouge', GETDATE(), 120000, 1),
             (N'Verte', DATEADD(hh, 1, GETDATE()), 90000, 0)
 
@@ -24,7 +24,7 @@ IF NOT EXISTS(
 	FROM [dbo].[modele]	
 )
 BEGIN
-	INSERT INTO [parc_auto].[dbo].[modele] ([marque], [modele], [vitesseMax], [poids], [puissance], [anneeSortieUsine],[type],[place],[energie])
+	INSERT INTO [parc_auto].[dbo].[modele] ([marque_vehicule], [modele_vehicule], [vitesse_max_vehicule], [poids_vehicule], [puissance_vehicule], [annee_sortie_usine_vehicule],[type_vehicule],[place_vehicule],[energie_vehicule])
     VALUES  (N'mercedes', N'classe_A', 320, 1500,300,2018,N'sport',4,N'essence'),
             (N'bmw', N'M5', 280, 1300,240,2015,N'break',4,N'gazole')
 
@@ -41,7 +41,7 @@ IF NOT EXISTS(
 	FROM [dbo].[client]	
 )
 BEGIN
-	INSERT INTO [parc_auto].[dbo].[client] ([nom], [prenom], [adresse], [telephone], [typePermis])
+	INSERT INTO [parc_auto].[dbo].[client] ([nom_client], [prenom_client], [adresse_client], [telephone_client], [type_permis_client])
     VALUES  (N'Sarkozy', N'Nicolas', N'10 rue du general foy, Amiens 80000', N'0607080910',N'B')
            
 
