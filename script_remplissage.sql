d8e87a46e0267fb809c4ce76903b97c874b7b763
LucasFournier-GIT <71459382+LucasFournier-GIT@users.noreply.github.com>
2020-11-13 19:11:25 +0100

    Update script_remplissage.sql

--- script_remplissage.sql
+++ script_remplissage.sql
@@ -1,12 +1,16 @@
 --En cours d'édition--
+USE parc_auto
+
+GO
+
 IF NOT EXISTS (
     SELECT TOP 1 [id]
     FROM [dbo].[vehicule]
 )
 BEGIN
     INSERT INTO [parc_auto].[dbo].[vehicule] ([couleur_vehicule], [date_premiere_circulation], [kilometre_vehicule], [disponibilite_vehicule])
-    VALUES  (N'Rouge', GETDATE(), 120000, TRUE),
-            (N'Verte', DATEADD(hh, 1, GETDATE()), 90000, FALSE)
+    VALUES  (N'Rouge', GETDATE(), 120000, 1),
+            (N'Verte', DATEADD(hh, 1, GETDATE()), 90000, 0)
 
     DELETE FROM [dbo].[vehicule]
     WHERE       [id] = 1;
