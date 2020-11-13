6f0850dfdede92cd122fe66212f5d8539476512f
LucasFournier-GIT <71459382+LucasFournier-GIT@users.noreply.github.com>
2020-11-13 19:10:11 +0100

    Update script_remplissage.sql

--- script_remplissage.sql
+++ script_remplissage.sql
@@ -4,7 +4,7 @@ IF NOT EXISTS (
     FROM [dbo].[vehicule]
 )
 BEGIN
-    INSERT INTO [parc_auto].[dbo].[vehicule] ([couleur], [date_premiere_circulation], [kilometrage], [disponibilite])
+    INSERT INTO [parc_auto].[dbo].[vehicule] ([couleur_vehicule], [date_premiere_circulation], [kilometre_vehicule], [disponibilite_vehicule])
     VALUES  (N'Rouge', GETDATE(), 120000, TRUE),
             (N'Verte', DATEADD(hh, 1, GETDATE()), 90000, FALSE)
 
