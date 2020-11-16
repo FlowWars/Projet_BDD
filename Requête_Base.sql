0fc60c2d332cba23c329554b8596ad05412101a8
LucasFournier-GIT <71459382+LucasFournier-GIT@users.noreply.github.com>
2020-11-16 18:39:04 +0100

    Rectification de certaines variables

--- Requête_Base.sql
+++ Requête_Base.sql
@@ -73,10 +73,10 @@ GO
 
 	CREATE TABLE [dbo].[location](
         [id] INT IDENTITY(1,1) PRIMARY KEY,
-        [idVehicule] INT /*clé secondaire */,
-        [idClient] INT /*clé secondaire*/,
-        [dateDebutLocation] DATETIME2,
-        [DateFinLocation] DATETIME2,
+        [idVehicule] INT /*clÃ© secondaire */,
+        [idClient] INT /*clÃ© secondaire*/,
+        [date_debut_location] DATETIME2,
+        [date_fin_location] DATETIME2,
         [disponibilite] BIT,
  
     )
