cfd63175e3d1e387798a0ce618e64a326c3d2bbe
FlowWars <65347962+FlowWars@users.noreply.github.com>
2020-11-23 14:13:40 +0100

    Modif de la ligne from

--- Projet_BDD-main/Vues ClientsInfo.sql
+++ Projet_BDD-main/Vues ClientsInfo.sql
@@ -1,7 +1,7 @@
 CREATE OR ALTER VIEW [dbo].[ClientsInfo]
 AS
     SELECT [id_client], [nom_client], [prenom_client], [adresse_client], [telephone_client], [type_permis_client]
-    FROM [dbo].[client_CLI] AS [PST] (NOLOCK)
+    FROM [dbo].[client_CLI]
 
 GO
 
