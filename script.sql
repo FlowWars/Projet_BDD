03e60b3f181102b205ab6d6fb57fb3ca27bcfdda
leo-paul-ciron <73255341+leo-paul-ciron@users.noreply.github.com>
2020-11-23 15:04:31 +0100

    ajout de vue pour chaque table (celui la lucas )

--- script.sql
+++ script.sql
@@ -89,52 +89,9 @@ GO
 	ADD CONSTRAINT FK_VEH_IDMOD FOREIGN KEY([id_modele_fk])
 	REFERENCES [dbo].[modele_MOD]([id_modele])
 GO
-*/
 
-IF NOT EXISTS (
-    SELECT TOP 1 [id_vehicule]
-    FROM [dbo].[vehicule_VEH]
-)
-BEGIN
-    INSERT INTO [parc_auto].[dbo].[vehicule_VEH] ([couleur_vehicule], [premiere_mise_en_circulation], [kilometre_vehicule], [disponibilite_vehicule])
-    VALUES  (N'Rouge', GETDATE(), 120000, 1),
-            (N'Verte', DATEADD(hh, 1, GETDATE()), 90000, 0)
 
-    DELETE FROM [dbo].[vehicule_VEH]
-    WHERE       [id_vehicule] = 1;
-END
 
-GO
-
-IF NOT EXISTS(
-	SELECT TOP 1 [id_modele]
-	FROM [dbo].[modele_MOD]	
-)
-BEGIN
-	INSERT INTO [parc_auto].[dbo].[modele_MOD] ([marque_vehicule], [modele_vehicule], [vitesse_max_vehicule], [poids_vehicule], [puissance_vehicule], [annee_sortie_usine_vehicule],[type_vehicule],[place_vehicule],[energie_vehicule])
-    VALUES  (N'mercedes', N'classe_A', 320, 1500,300,N'2018-10-23',N'sport',4,N'essence'),
-            (N'bmw', N'M5', 280, 1300,240,N'2015-02-17',N'break',4,N'gazole')
-
-    DELETE FROM [dbo].[modele_MOD]
-    WHERE       [id_modele] = 1;
-
-END
-GO
-
-IF NOT EXISTS(
-	SELECT TOP 1 [id_client]
-	FROM [dbo].[client_CLI]	
-)
-BEGIN
-	INSERT INTO [parc_auto].[dbo].[client_CLI] ([nom_client], [prenom_client], [adresse_client], [telephone_client], [type_permis_client])
-    VALUES  (N'Sarkozy', N'Nicolas', N'10 rue du general foy, Amiens 80000', N'0607080910',N'B')
-           
-
-    DELETE FROM [dbo].[client_CLI]
-    WHERE       [id_client] = 1;
-
-END
-/*
 GO
 	ALTER TABLE [dbo].[location_LOC]
 	ADD CONSTRAINT FK_LOC_VEH FOREIGN KEY([id_vehicule_fk])
@@ -165,7 +122,7 @@ WHERE [id_vehicule]=(SELECT [id_vehicule_fk] FROM [location_LOC] WHERE [disponib
 GO
 
 
-
+/*
 GO
 	ALTER TABLE [dbo].[vehicule_VEH]
 	ADD [kilometre_acquisition] DATETIME2
@@ -176,4 +133,76 @@ GO
 	ALTER TABLE [dbo].[vehicule_VEH]
 	DROP COLUMN [kilometre_vehicule]
 GO
+*/
+
+IF NOT EXISTS (
+    SELECT TOP 1 [id_vehicule]
+    FROM [dbo].[vehicule_VEH]
+)
+BEGIN
+    INSERT INTO [parc_auto].[dbo].[vehicule_VEH] ([couleur_vehicule],[id_modele_fk] , [premiere_mise_en_circulation], [kilometre_acquisition], [disponibilite_vehicule])
+	VALUES  (N'Rouge', 2,CAST('2007-05-08 12:35:29. 1234567 +12:15' AS datetime2(7)), 1200, 1),
+            (N'Verte', 1,CAST('2007-05-08 12:35:29. 1234567 +12:15' AS datetime2(7)) , 1500.35, 0)
+
+   
+END
+
+GO
+
+IF NOT EXISTS(
+	SELECT TOP 1 [id_modele]
+	FROM [dbo].[modele_MOD]	
+)
+BEGIN
+	INSERT INTO [parc_auto].[dbo].[modele_MOD] ([marque_vehicule], [modele_vehicule], [vitesse_max_vehicule], [poids_vehicule], [puissance_vehicule], [annee_sortie_usine_vehicule],[type_vehicule],[place_vehicule],[energie_vehicule])
+    VALUES  (N'mercedes', N'classe_A', 320, 1500,300,N'2018-10-23',N'sport',4,N'essence'),
+            (N'bmw', N'M5', 280, 1300,240,N'2015-02-17',N'break',4,N'gazole')
+
+   
+
+END
+GO
+
+IF NOT EXISTS(
+	SELECT TOP 1 [id_client]
+	FROM [dbo].[client_CLI]	
+)
+BEGIN
+	INSERT INTO [parc_auto].[dbo].[client_CLI] ([nom_client], [prenom_client], [adresse_client], [telephone_client], [type_permis_client])
+    VALUES  (N'Sarkozy', N'Nicolas', N'10 rue du general foy, Amiens 80000', N' 0607080910',N'B'),
+			(N'Holland', N'Francois', N'9 rue du general foy, Amiens 80000', N' 0758962512',N'B')
+           
+
+
+END
+GO
+
+CREATE OR ALTER VIEW [dbo].[VehiculeInfo]
+AS
+    SELECT *
+    FROM [dbo].[vehicule_VEH]
+
+GO
+
+SELECT * FROM [dbo].[VehiculeInfo]
+
+GO
+
+CREATE OR ALTER VIEW [dbo].[ClientsInfo]
+AS
+    SELECT *
+    FROM [dbo].[client_CLI]
+
+GO
+SELECT * FROM [dbo].[ClientsInfo]
+
+GO
+CREATE OR ALTER VIEW [dbo].[ModeleInfo]
+AS
+    SELECT *
+    FROM [dbo].[modele_MOD]
+
+GO
+
+SELECT * FROM [dbo].[ModeleInfo]
 
