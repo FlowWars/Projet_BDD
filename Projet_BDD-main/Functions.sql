4ff54556c49bb7c60d8582bd6613039c04c6f699
LucasFournier-GIT <71459382+LucasFournier-GIT@users.noreply.github.com>
2020-12-21 21:27:00 +0100

    Révision entière de la fonction

--- Projet_BDD-main/Functions.sql
+++ Projet_BDD-main/Functions.sql
@@ -2,14 +2,15 @@ USE [parc_auto]
 
 GO
 
-CREATE OR ALTER FUNCTION [dbo].[KmDepuisAcquisition]()
+CREATE OR ALTER FUNCTION [dbo].[KmDepuisAcquisition](@id_vehicule INT)
 RETURNS FLOAT
 AS
 BEGIN
     DECLARE @Resultat FLOAT
 
-    SELECT @Resultat = [kilometre_vehicule] - [kilometre_acquisition]
-    FROM [dbo].[vehicule_VEH]
+    SELECT @Resultat = SUM(kilometres_parcourus)
+    FROM [dbo].[location_LOC]
+	WHERE [id_vehicule_fk] = @id_vehicule
 
     RETURN(@Resultat)
 END
